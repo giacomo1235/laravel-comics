e9fead1c5117010f6c6e9d12f942fe18e7c47a67
giacomo1235 <giacomo.passariello@gmail.com>
2022-04-27 22:59:17 +0200

    funziona

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,17 +1,17 @@
 <header>
     <div class="container">
-        <img src="./images/dc-logo.png" alt="logo" />
+        <a href="{{route('home')}}"><img src="./images/dc-logo.png" alt="logo" /></a>
         <ul>
-          <li><a href="#!">characters</a></li>
-          <li><a href="#!">comics</a></li>
-          <li><a href="#!">movies</a></li>
-          <li><a href="#!">tv</a></li>
-          <li><a href="#!">games</a></li>
-          <li><a href="#!">collectibles</a></li>
-          <li><a href="#!">videos</a></li>
-          <li><a href="#!">fans</a></li>
-          <li><a href="#!">news</a></li>
-          <li><a href="#!">shop</a></li>
+          <li><a href="{{route('characters')}}">characters</a></li>
+          <li><a href="{{route('comics')}}">comics</a></li>
+          <li><a href="{{route('movies')}}">movies</a></li>
+          <li><a href="{{route('tv')}}">tv</a></li>
+          <li><a href="{{route('games')}}">games</a></li>
+          <li><a href="{{route('collectibles')}}">collectibles</a></li>
+          <li><a href="{{route('videos')}}">videos</a></li>
+          <li><a href="{{route('fans')}}">fans</a></li>
+          <li><a href="{{route('news')}}">news</a></li>
+          <li><a href="{{route('shop')}}">shop</a></li>
         </ul>
     </div>
 </header>
