6e97dd6da4b2b081b21656bff6533b89d42d7829
giacomo1235 <giacomo.passariello@gmail.com>
2022-04-28 13:37:30 +0200

    indentazione

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -1,42 +1,30 @@
+@php
+  $arrLinks = config('footermenu')   
+@endphp  
 <footer>
     <div class="main-footer">
       <div class="col-left">
         <div class="col-4">
           <ul id="dcComics">
-          @php
-            $arrLinks = config('footermenu')   
-          @endphp  
-          @foreach ($arrLinks as $link)
-          {{-- @dd($link) --}}
-          <ul>
-            <li class="title-li">dc comics</li>
-            @foreach ($link['linkComics'] as $item)
-            <li><a href="{{$item['href']}}">{{$item['text']}}</a></li>   
+            @foreach ($arrLinks as $link)
+              <ul>
+                <li class="title-li">dc comics</li>
+                @foreach ($link['linkComics'] as $item)
+                  <li><a href="{{$item['href']}}">{{$item['text']}}</a></li>   
+                @endforeach
+              </ul>
             @endforeach
           </ul>
-    
-          @endforeach
-          {{-- @dd($arrLinks) --}}
-          {{-- <li class="title-li">dc comics</li>
-          <li><a href="#!">Characters</a></li>
-          <li><a href="#!">Comics</a></li>
-          <li><a href="#!">Movies</a></li>
-          <li><a href="#!">Tv</a></li>
-          <li><a href="#!">Games</a></li>
-          <li><a href="#!">Collectibles</a></li>
-          <li><a href="#!">Videos</a></li>
-          <li><a href="#!">News</a></li> --}}
-        </ul>
-        <ul id="shop">
-          @foreach ($arrLinks as $link)
-          <ul>
-            <li class="title-li">shop</li>
-            @foreach ($link['linksShop'] as $item)
-            <li><a href="{{$item['href']}}">{{$item['text']}}</a></li>   
+          <ul id="shop">
+            @foreach ($arrLinks as $link)
+              <ul>
+                <li class="title-li">shop</li>
+                @foreach ($link['linksShop'] as $item)
+                  <li><a href="{{$item['href']}}">{{$item['text']}}</a></li>   
+                @endforeach
+              </ul>
             @endforeach
           </ul>
-          @endforeach
-        </ul>
         </div>
         <div class="col-4">
           <ul class="dc">
@@ -48,7 +36,7 @@
               @endforeach
             </ul>
             @endforeach
-        </ul>
+          </ul>
         </div>
         <div class="col-4">
           <ul class="sites">
@@ -60,7 +48,7 @@
               @endforeach
             </ul>
             @endforeach
-        </ul>
+          </ul>
         </div>
       </div>
       <div class="col-right">
